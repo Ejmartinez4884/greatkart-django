d5c88918ba54e084fff526c185d24197c9e6bf1c
ericmartinez <eric.martinez4884@gmail.com>
2024-06-13 23:51:31 -0500

    Working activation email

--- roseraedesigns_site/settings.py
+++ roseraedesigns_site/settings.py
@@ -136,3 +136,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'eric.martinez4884@gmail.com'
+EMAIL_HOST_PASSWORD = 'rxepjxbmopmpvijf'
+EMAIL_USE_TLS = True
